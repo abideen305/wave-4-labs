6a6f03981849036c15c5be0d797efd182211a855
abideen305 <abideenmuhammed2018@gmail.com>
2020-08-17 17:33:08 +0100

    update to function

---
+++ scripting/power_e.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#It's your turn to import and use the math module. Use the math module to calculate e to the power of 3. print the answer.
+# print e to the power of 3 using the math module
+import math
+
+power_e = math.e**3
+power_e
+
